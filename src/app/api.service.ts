ef56fec68d7aa05e2c9d3e4c2f9967bd9c6fbd53
bart-khalid <60948931+bart-khalid@users.noreply.github.com>
2023-05-11 01:54:29 +0100

    1 file modified

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,22 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-// const API_URL = 'https://reqres.in';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  // public get(url): Observable<any> {
-  //   return this.http.get(API_URL + '/api/' + url).pipe(map((res) => res));
-  // }
-
   public login(): Observable<any> {
     return this.http
-      .post('http://102.50.254.72:8010/jwt-api-token-auth/', {
-        username: 'amhaoud',
-        password: 'amhaoud123',
+      .post('http:api_endpoint...', {
+        username: 'admin',
+        password: 'admin',
       })
       .pipe(map((res) => res));
   }
